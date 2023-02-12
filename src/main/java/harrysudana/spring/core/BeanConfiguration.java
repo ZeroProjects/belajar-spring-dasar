a21c5c9fd4996198444873b1c334d5603d195f98
hasu@m93p <harrysudana@gmail.com>
2023-02-12 15:40:43 +0800

    Tambah log untuk melihat singelton

--- src/main/java/harrysudana/spring/core/BeanConfiguration.java
+++ src/main/java/harrysudana/spring/core/BeanConfiguration.java
@@ -4,13 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import harrysudana.spring.core.data.Foo;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Configuration
 public class BeanConfiguration {
 
     @Bean
     public Foo foo(){
         Foo foo = new Foo();
+        log.info("Create new foo");
         return foo;
     }
 }
