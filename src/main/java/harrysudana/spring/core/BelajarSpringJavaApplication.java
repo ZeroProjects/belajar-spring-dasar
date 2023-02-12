f313b0d2bba747e45de4d8854f842e037ea486e4
hasu@m93p <harrysudana@gmail.com>
2023-02-12 14:34:24 +0800

    Spring Initializr
    https://start.spring.io

---
+++ src/main/java/harrysudana/spring/core/BelajarSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package harrysudana.spring.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/harrysudana/spring/core/BelajarSpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package harrysudana.spring.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
